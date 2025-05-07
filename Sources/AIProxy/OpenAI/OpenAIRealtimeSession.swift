9b74bde77e7077ed1cde243bc5e793d283d5628f
Sergey Borovkov <sergeyborovkov364@gmail.com>
2025-05-07 14:12:02 +0200

    new commit

--- Sources/AIProxy/OpenAI/OpenAIRealtimeSession.swift
+++ Sources/AIProxy/OpenAI/OpenAIRealtimeSession.swift
@@ -124,6 +124,8 @@ open class OpenAIRealtimeSession {
         }
         logIf(.debug)?.debug("Received \(messageType) from OpenAI")
 
+        print("---> messageType: \(messageType)")
+
         switch messageType {
         case "error":
             let errorBody = String(describing: json["error"] as? [String: Any])
